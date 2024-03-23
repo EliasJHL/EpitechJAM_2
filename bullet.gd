da2e154590638e92477b0de81b0c8392b164012f
Elias Josué HAJJAR LLAUQUEN <145333474+EliasJHL@users.noreply.github.com>
2024-03-23 02:57:07 +0100

    [ADD] basic portal system added & timeout of bullets added

--- bullet.gd
+++ bullet.gd
@@ -1,5 +1,6 @@
 extends RigidBody2D
 
+const portal_path = preload("res://blue_portal.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -14,7 +15,16 @@ func _process(delta):
 func _on_body_entered(body):
 	pass
 
-
+#Gestion de contact avec un mur compatible
 func _on_area_2d_body_entered(body):
 	if body.is_in_group("Portail_OK"):
+		var portal = portal_path.instantiate()
+		portal.global_position = self.global_position
+		portal.global_rotation = self.global_rotation
+		get_tree().get_root().add_child(portal)
 		queue_free()
+		
+
+#Ajout d'un timer pour que la balle despawn au bout de 5s
+func _on_timer_timeout():
+	queue_free()
