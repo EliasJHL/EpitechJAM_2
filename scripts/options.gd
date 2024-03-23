3c930745fb0189807953f770f28625da052b0a0e
Elias Josué HAJJAR LLAUQUEN <145333474+EliasJHL@users.noreply.github.com>
2024-03-23 15:22:38 +0100

    [UPDATE] options butons disabled

--- scripts/options.gd
+++ scripts/options.gd
@@ -16,19 +16,14 @@ func _on_back_pressed():
 
 
 func _on_left_config_pressed():
-	# Afficher un message pour indiquer à l'utilisateur qu'il peut maintenant entrer la nouvelle touche
-	print("Cliquez sur une touche pour changer le keybind...")
+	pass
+	#var old_keybind = InputMap.action_get_events("Left") # Récupère le premier keybind de l'action
 
-	# Attendre que l'utilisateur appuie sur une touche
-	var old_keybind = InputMap.action_get_events("Left") # Récupère le premier keybind de l'action
+	#InputMap.erase_action("Left")
+	#await InputEventKey
 
-	# Effacer l'ancien keybind pour éviter les doublons
-	InputMap.erase_action("Left")
-	await InputEventKey # Attend un nouvel événement d'entrée
-
-	var key = InputEventKey
-	print(key)
-	# Mettre à jour le keybind avec la nouvelle touche choisie par l'utilisateur
+	#var key = InputEventKey
+	#print(key)
 	#var new_key = get_tree().input.get_action_strength(get_name()) > 0
 	#InputMap.add_action("Left", new_key)
-	#print("Nouveau keybind pour", get_name(), ":", new_key)
+	
