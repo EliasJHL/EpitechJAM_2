9d9d74ee1d48774c0ef7e57dbb2500ab78f85acf
Elias Josué HAJJAR LLAUQUEN <145333474+EliasJHL@users.noreply.github.com>
2024-03-23 04:32:33 +0100

    [KEYBIND] keybind added

--- CharacterBody2D.gd
+++ CharacterBody2D.gd
@@ -68,10 +68,10 @@ func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("Jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	var direction = Input.get_axis("ui_left", "ui_right")
+	var direction = Input.get_axis("Left", "Right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
@@ -83,4 +83,5 @@ func _physics_process(delta):
 
 #Timer ajouté pour ne pas pouvoir spam
 func _on_timer_timeout():
-	pass
+		pass
+
