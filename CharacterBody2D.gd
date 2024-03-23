ae1cf67f539af6740fa59ee7cafae4b4d30ce2fe
Elias Josué HAJJAR LLAUQUEN <145333474+EliasJHL@users.noreply.github.com>
2024-03-23 01:17:05 +0100

    [ADD] basic scene to test the player & player movement fixed & weapon angle added

--- CharacterBody2D.gd
+++ CharacterBody2D.gd
@@ -4,21 +4,27 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+#Fonction pour le viseur de l'arme
+func _process(delta):
+	var target_position = get_global_mouse_position()
+	#Regard sur la souris → Il est basé sur le centre du joueur
+	$Weapon.look_at(target_position)
 
+#Fonction pour placer les portails
+func fire():
+	if Input.is_action_pressed("Place_Portal"):
+		print("fire")
+
+#Système de gravité et déplacements basiques
 func _physics_process(delta):
-	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
-	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
