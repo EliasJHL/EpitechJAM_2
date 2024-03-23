93b212420d69035bd6ddc22817a180c66fdcf143
Elias Josué HAJJAR LLAUQUEN <145333474+EliasJHL@users.noreply.github.com>
2024-03-23 02:01:52 +0100

    [ADD][ENCHANCE][FIX] fire system enchanced & bug fix

--- CharacterBody2D.gd
+++ CharacterBody2D.gd
@@ -6,17 +6,24 @@ const JUMP_VELOCITY = -400.0
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+#assets dont j'ai besoin pour l'arme
+const bullet_path = preload("res://bullet.tscn")
+
 #Fonction pour le viseur de l'arme
 func _process(delta):
 	var target_position = get_global_mouse_position()
 	#Regard sur la souris → Il est basé sur le centre du joueur
-	$Weapon.look_at(target_position)
+	$Weapon_rotation.look_at(target_position)
 
 #Fonction pour placer les portails
 func fire():
 	if Input.is_action_just_pressed("Place_Portal"):
-		print("fire")
-
+		#création d'une instance de la balle
+		var bullet = bullet_path.instantiate()
+		bullet.global_position = $Weapon_rotation/Weapon/Shoot_Point.global_position
+		#Ajout des clones des balles sur la scene
+		get_tree().get_root().add_child(bullet)
+		
 #Système de gravité et déplacements basiques
 func _physics_process(delta):
 	if not is_on_floor():
