a07691e1d7886c8c2b88f1bcc8e2633799087bdc
Elias Josué HAJJAR LLAUQUEN <145333474+EliasJHL@users.noreply.github.com>
2024-03-24 21:31:56 +0100

    [FIX] menu load level

--- scripts/start_menu.gd
+++ scripts/start_menu.gd
@@ -12,7 +12,7 @@ func _process(delta):
 
 
 func _on_start_button_pressed():
-	get_tree().change_scene_to_file("res://scenes/main_scene.tscn")
+	get_tree().change_scene_to_file("res://scenes/level.tscn")
 
 
 func _on_options_button_pressed():
